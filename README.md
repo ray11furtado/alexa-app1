e00deba3d4e3dea972d905ae1e5994743c44493c
Ray Furtado <rayfurtado11@gmail.com>
2018-01-13 10:39:29 -0500

    basic app working

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Alexa skill app that uses Python to reads top posts of any given sub reddit! <br />
 Create file account_info.py w/ USERNAME and PASSWORD for a Reddit account <br />
-If uploading to github be sure account_info is in .gitignore!
+If uploading to github be sure account_info is in .gitignore! <br />
+Inside reddit_reader, input desired subreddit
+Use ngrok to host flask
--- reddit_reader.py
+++ reddit_reader.py
@@ -7,7 +7,59 @@ import time
 import unidecode
 
 app = Flask(__name__)
-app = Ask(app, "/reddit_reader")
+ask = Ask(app, "/reddit_reader")
+subreddit = "hockey"
+reddit_url = "https://www.reddit.com"
+reddit_api_url = "{}/api/login".format(reddit_url)
 
-def get_headlines():
-    pass
+
+
+def get_reddit_data():
+    user_pass_dict = {'user': USERNAME,
+                      'passwd': PASSWORD,
+                      'api_type': 'json'}
+    sess = requests.Session()
+    sess.headers.update({'User-Agent': 'Testing Alexa: Ray'})
+    print(reddit_api_url)
+    sess.post(reddit_api_url, data=user_pass_dict)
+    # REDDIT API is picky when making rapid API calls
+    time.sleep(1)
+    url = "{}/r/{}/.json?limit=5".format(reddit_url, subreddit)
+    html = sess.get(url)
+    data = json.loads(html.content.decode("utf-8"))
+    titles = [unidecode.unidecode(listing['data']['title']) for listing in data['data']['children']] # flake8: noqa
+    titles = '...'.join([i for i in titles])
+    return titles
+
+
+
+# Welcome start skill for Alexa
+
+
+@ask.launch
+def start_skill():
+    # s = "Would you like me to read the headlines for {}".format(subreddit)
+    s = "Would you like me to tell you who the best fifa player is?"
+    return question(s)
+
+
+# Handles if user says yes
+
+
+@ask.intent("YesIntent")
+def share_headlines():
+    headlines = get_reddit_data()
+    headline_msg = "Current headlines are {}".format(headlines)
+    # return statement(headline_msg)
+    return statement("It's not toby thats for sure!")
+
+
+@ask.intent("NoIntent")
+def no_intent():
+    bye = "Okay then, bye"
+    return statement(bye)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
